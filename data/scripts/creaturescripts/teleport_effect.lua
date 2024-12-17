e04218787d69f590ccf972d799c4ee6d80aaee1b
Marko999x <61252018+DoktorHolzbein@users.noreply.github.com>
2024-12-17 23:16:51 +0100

    fix monster.xml again and teleport effects text position

--- data/scripts/creaturescripts/teleport_effect.lua
+++ data/scripts/creaturescripts/teleport_effect.lua
@@ -2,15 +2,17 @@ local effects = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,
 local colorS = {TEXTCOLOR_RED, TEXTCOLOR_LIGHTGREEN, TEXTCOLOR_LIGHTBLUE, TEXTCOLOR_YELLOW}	
 	
 local effects = {
-	{position = Position(1000, 998, 7), text = 'WELCOME', effect = 38, color = math.random(1, 256)},
+	{position = Position(1000, 1001, 7), text = 'WELCOME', effect = 38, color = math.random(1, 256)},
 
-	{position = Position(1006, 997, 7), text = 'WAR ARENA', effect = 38, color = math.random(1, 256)},
-	{position = Position(1006, 998, 7), text = 'HUNTING', effect = 38, color = math.random(1, 256)},
-	{position = Position(1006, 999, 7), text = 'QUEST', effect = 38, color = math.random(1, 256)},
+	{position = Position(993, 999, 7), text = 'ARENA', effect = 38, color = math.random(1, 256)},
+	{position = Position(993, 1000, 7), text = 'QUESTS', effect = 38, color = math.random(1, 256)},
+	{position = Position(993, 1001, 7), text = 'HUNTINGS', effect = 38, color = math.random(1, 256)},
+	{position = Position(993, 1002, 7), text = 'CITIES', effect = 38, color = math.random(1, 256)},
 	
-	{position = Position(994, 997, 7), text = 'SHOP', effect = 38, color = math.random(1, 256)},
-	{position = Position(994, 998, 7), text = 'TRAINING', effect = 38, color = math.random(1, 256)},
-	{position = Position(994, 999, 7), text = 'DEPOT', effect = 38, color = math.random(1, 256)},
+	{position = Position(1007, 999, 7), text = 'SHOP', effect = 38, color = math.random(1, 256)},
+	{position = Position(1007, 1000, 7), text = 'DEPOT', effect = 38, color = math.random(1, 256)},
+	{position = Position(1007, 1001, 7), text = 'EVENTS', effect = 38, color = math.random(1, 256)},
+	{position = Position(1007, 1002, 7), text = 'TRAINERS', effect = 38, color = math.random(1, 256)},
 }
 
 local tileeffect = GlobalEvent("Tile Effect")
